54bcaec9b430ab88939719376c5b2f384bbe5c18
liujie <839899700@qq.com>
2018-10-10 15:14:55 +0800

    update repository

--- src/main/java/com/example/elastic/service/mybatic/CustomerRepository.java
+++ src/main/java/com/example/elastic/service/mybatic/CustomerRepository.java
@@ -2,7 +2,12 @@ package com.example.elastic.service.mybatic;
 
 import com.example.elastic.entity.JJRCustomers;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.query.Param;
+
+import javax.annotation.PostConstruct;
 import java.util.List;
 
 
@@ -10,7 +15,9 @@ public interface CustomerRepository extends Repository<JJRCustomers,String> {
 
     List<JJRCustomers> findByUserId(long userId);
 
-
-
     List<JJRCustomers> queryAllByUserIdBetween(long startUserId, long endUserId);
+
+    @Modifying
+    @Query("update t_jjr_customer cc set cc.deleted = 1 where cc.user_id = :userId and cc.agent_id :agentId")
+    int updateByUserIdAndAgentId(@Param("userId") long userId,@Param("agentId") String agentId);
 }
