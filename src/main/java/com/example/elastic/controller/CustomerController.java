2ec689e6ff595a6fae0e3a51d13fcaf5897b2148
liujie <839899700@qq.com>
2018-10-10 18:09:37 +0800

    update

--- src/main/java/com/example/elastic/controller/CustomerController.java
+++ src/main/java/com/example/elastic/controller/CustomerController.java
@@ -10,11 +10,15 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("customer")
 public class CustomerController {
 
     @Autowired
@@ -23,7 +27,7 @@ public class CustomerController {
     @Autowired
     CustomerElasticRepository customerElasticRepository ;
 
-    @GetMapping("/customer")
+    @GetMapping("/save")
     public String queryCustomer(long startUserId,long endUserId){
         List<JJRCustomers> list = customerRepository.queryAllByUserIdBetween(startUserId,endUserId);
         list.stream().forEach(item -> {
@@ -36,12 +40,18 @@ public class CustomerController {
 
     @GetMapping("/query")
     public String query(String agentId){
-//        Optional<JJRCustomers> cc  = customerElasticRepository.findById("1483319acbbb4ec6849350b267daf358");
+        Optional<JJRCustomers> cc  = customerElasticRepository.findById("1483319acbbb4ec6849350b267daf358");
         QueryBuilder builder = QueryBuilders.matchPhraseQuery("agentId",agentId);
         Pageable pageable = PageRequest.of(0,100, Sort.Direction.ASC,"userId");
         Iterable<JJRCustomers> c = customerElasticRepository.search(builder,pageable);
 
         List<JJRCustomers> customers = customerElasticRepository.findJJRCustomersByAgentIdEquals(agentId,pageable);
         return "boolean";
+    }
+
+    @RequestMapping("/update")
+    public String updateAgentId(@RequestParam("agentId") String agenId,@RequestParam("userId") long userId){
+        int update = customerRepository.updateByUserIdAndAgentId(userId,agenId);
+        return "success";
     }
 }
--- src/main/java/com/example/elastic/service/mybatic/CustomerRepository.java
+++ src/main/java/com/example/elastic/service/mybatic/CustomerRepository.java
@@ -2,25 +2,26 @@ package com.example.elastic.service.mybatic;
 
 import com.example.elastic.entity.JJRCustomers;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.annotation.PostConstruct;
 import java.util.List;
 
 
-public interface CustomerRepository extends JpaRepository<JJRCustomers,String> {
+public interface CustomerRepository extends Repository<JJRCustomers,String> {
 
     List<JJRCustomers> findByUserId(long userId);
 
     List<JJRCustomers> queryAllByUserIdBetween(long startUserId, long endUserId);
 
+    //update 需要添加事务
+    @Transactional
     @Modifying
-    @Query("update JJRCustomers  as cc set cc.deleted = 1 where cc.user_id = :userId and cc.agent_id = :agentId")
-    int deleteByUserIdAndAgentId(@Param("userId") long userId,@Param("agentId") String agentId);
+    @Query("update JJRCustomers  as cc set cc.agentId = :agentId where cc.userId = :userId")
+    int updateByUserIdAndAgentId(@Param("userId") long userId,@Param("agentId") String agentId);
 
 
 
